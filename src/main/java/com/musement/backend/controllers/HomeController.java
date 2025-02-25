fa55cdbe3764ba00e76221ef2e408d3212ac843e
Malysheva Anastasia <anastasiamalysheva250@gmail.com>
2025-02-25 09:32:53 +0300

    add CoreConfig

--- src/main/java/com/musement/backend/controllers/HomeController.java
+++ src/main/java/com/musement/backend/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package com.musement.backend.controllers;
 
 import com.musement.backend.models.User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+//@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {})
 public class HomeController {
     @GetMapping
     public String home() {
