76c9363fc6ed227ef8b1d9db796aa9f860275f67
Anna Artamonova <anna.artamono@gmail.com>
2025-02-26 04:31:37 +0300

    fix bug in ConcertController

---
+++ src/test/java/com/musement/backend/controllers/ConcertControllerTest.java
@@ -0,0 +1,53 @@
+package com.musement.backend.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConcertControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllConcerts() {
+    }
+
+    @Test
+    void getConcertById() {
+    }
+
+    @Test
+    void getAttendees() {
+    }
+
+    @Test
+    void isUserAttendingConcert() {
+    }
+
+    @Test
+    void getConcertsByAttendee() {
+    }
+
+    @Test
+    void searchConcerts() {
+    }
+
+    @Test
+    void createConcert() {
+    }
+
+    @Test
+    void addAttendeeToConcert() {
+    }
+
+    @Test
+    void updateConcert() {
+    }
+
+    @Test
+    void removeAttendeeFromConcert() {
+    }
+}
