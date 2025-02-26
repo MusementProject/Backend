26d7607c839e8751c9ef2c1efd9dd8e5b5b100fd
Anna Artamonova <anna.artamono@gmail.com>
2025-02-26 04:14:09 +0300

    add more methods to Concert classes

--- src/main/java/com/musement/backend/repositories/ConcertRepository.java
+++ src/main/java/com/musement/backend/repositories/ConcertRepository.java
@@ -2,8 +2,23 @@ package com.musement.backend.repositories;
 
 import com.musement.backend.models.Concert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface ConcertRepository extends JpaRepository<Concert, Long> {
+    @Query("SELECT c FROM Concert c JOIN c.attendees u WHERE u.id = :userId")
+    List<Concert> findByAttendee(@Param("userId") Long userId);
+
+    @Query("SELECT c FROM Concert c WHERE " +
+            "(:location IS NULL OR c.location = :location) AND " +
+            "(:fromDate IS NULL OR c.date >= :fromDate) AND " +
+            "(:toDate IS NULL OR c.date <= :toDate)")
+    List<Concert> findByFilters(@Param("location") String location,
+                                @Param("fromDate") LocalDate fromDate,
+                                @Param("toDate") LocalDate toDate);
 }
