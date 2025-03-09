46a9645d8d509825e76bbcfb1d733c6d53b8990d
Anna Artamonova <anna.artamono@gmail.com>
2025-03-10 02:44:48 +0300

    implement Controllers and update SecurityConfig

--- src/main/java/com/musement/backend/controllers/LoginController.java
+++ src/main/java/com/musement/backend/controllers/LoginController.java
@@ -1,4 +1,26 @@
 package com.musement.backend.controllers;
 
+import com.musement.backend.dto.LoginRequestDTO;
+import com.musement.backend.dto.LoginResponseDTO;
+import com.musement.backend.services.LoginService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+@RequestMapping("/api/login")
 public class LoginController {
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @PostMapping
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO request) {
+        LoginResponseDTO response = loginService.login(request);
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/musement/backend/controllers/RegistrationController.java
+++ src/main/java/com/musement/backend/controllers/RegistrationController.java
@@ -1,4 +1,26 @@
 package com.musement.backend.controllers;
 
+import com.musement.backend.dto.RegistrationRequestDTO;
+import com.musement.backend.dto.RegistrationResponseDTO;
+import com.musement.backend.services.RegistrationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/register")
 public class RegistrationController {
+    private final RegistrationService registrationService;
+
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<RegistrationResponseDTO> register(@RequestBody RegistrationRequestDTO request) {
+        RegistrationResponseDTO response = registrationService.register(request);
+        return ResponseEntity.ok(response);
+    }
 }
